85d4c97c00ad66b943e57165c1745376fe5489b4
Michael Harris <mhguitar1997@gmail.com>
2019-08-23 10:02:58 -0700

    Create author service impl

---
+++ bookstore/src/main/java/com/lambdaschool/bookstore/services/AuthorServiceImpl.java
@@ -0,0 +1,22 @@
+package com.lambdaschool.bookstore.services;
+
+import com.lambdaschool.bookstore.models.Author;
+import com.lambdaschool.bookstore.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class AuthorServiceImpl implements AuthorService{
+    @Autowired
+    private AuthorRepository authorrepos;
+
+    @Override
+    public List<Author> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Author> assign(long bookid, long authorid) {
+        return null;
+    }
+}
