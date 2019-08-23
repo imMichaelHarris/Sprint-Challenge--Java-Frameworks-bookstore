69552545f9e87131d6f9be7e1062669cf34fd6d9
Michael Harris <mhguitar1997@gmail.com>
2019-08-23 08:21:27 -0700

    Add constructor and getters and setters to book model

--- bookstore/src/main/java/com/lambdaschool/bookstore/models/Book.java
+++ bookstore/src/main/java/com/lambdaschool/bookstore/models/Book.java
@@ -16,5 +16,44 @@ public class Book {
     private String isbn;
     private int copy;
 
+    public Book(String bookTitle, String isbn, int copy) {
+        this.bookTitle = bookTitle;
+        this.isbn = isbn;
+        this.copy = copy;
+    }
 
+    public Book() {
+    }
+
+    public long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(long bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getCopy() {
+        return copy;
+    }
+
+    public void setCopy(int copy) {
+        this.copy = copy;
+    }
 }
